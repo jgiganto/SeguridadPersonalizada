2201e2e46d9065040f3db6d9ab62b45f51baec88
jgiganto <jgiganto1@hotmail.com>
2018-02-19 10:56:51 +0100

    s

---
+++ SeguridadPersonalizada/Controllers/ValidacionController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SeguridadPersonalizada.Controllers
+{
+    public class ValidacionController : Controller
+    {
+        // GET: Validacion
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(String usuario,String password)
+        {
+            return View();
+        }
+
+    }
+}
